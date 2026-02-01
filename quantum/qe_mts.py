28233148cd1b31ea6f4bf64c94768de7abc54269
chtang <chtang@hmc.edu>
2026-02-01 05:23:09 -0500

    attempt to parallelization

--- quantum/qe_mts.py
+++ quantum/qe_mts.py
@@ -7,12 +7,17 @@ import matplotlib.pyplot as plt
 from classical.mts import MTS
 import tutorial_notebook.auxiliary_files.labs_utils as utils
 
+# utilize multiple qubit parallelization
+cudaq.set_target("nvidia", option="mqpu")
+
+
 ### CudaQ kernels for 2q and 4q terms ###
 @cudaq.kernel
 def r_zz(q0: cudaq.qubit, q1: cudaq.qubit, theta: float):
     cx(q0, q1)
     rz(theta, q1)
     cx(q0, q1)
+
 
 @cudaq.kernel
 def r_yz(q0: cudaq.qubit, q1: cudaq.qubit, theta: float):
@@ -25,6 +30,7 @@ def r_yz(q0: cudaq.qubit, q1: cudaq.qubit, theta: float):
     # Undo basis change
     s.adj(q0)
     h(q0)
+
 
 @cudaq.kernel
 def r_zy(q0: cudaq.qubit, q1: cudaq.qubit, theta: float):
@@ -40,11 +46,9 @@ def r_zy(q0: cudaq.qubit, q1: cudaq.qubit, theta: float):
 
 
 @cudaq.kernel
-def r_zzzz(q0: cudaq.qubit,
-          q1: cudaq.qubit,
-          q2: cudaq.qubit,
-          q3: cudaq.qubit,
-          theta: float):
+def r_zzzz(
+    q0: cudaq.qubit, q1: cudaq.qubit, q2: cudaq.qubit, q3: cudaq.qubit, theta: float
+):
     cx(q0, q1)
     cx(q1, q2)
     cx(q2, q3)
@@ -57,11 +61,9 @@ def r_zzzz(q0: cudaq.qubit,
 
 
 @cudaq.kernel
-def r_yzzz(q0: cudaq.qubit,
-           q1: cudaq.qubit,
-           q2: cudaq.qubit,
-           q3: cudaq.qubit,
-           theta: float):
+def r_yzzz(
+    q0: cudaq.qubit, q1: cudaq.qubit, q2: cudaq.qubit, q3: cudaq.qubit, theta: float
+):
     s(q0)
     h(q0)
 
@@ -72,11 +74,9 @@ def r_yzzz(q0: cudaq.qubit,
 
 
 @cudaq.kernel
-def r_zyzz(q0: cudaq.qubit,
-           q1: cudaq.qubit,
-           q2: cudaq.qubit,
-           q3: cudaq.qubit,
-           theta: float):
+def r_zyzz(
+    q0: cudaq.qubit, q1: cudaq.qubit, q2: cudaq.qubit, q3: cudaq.qubit, theta: float
+):
     s(q1)
     h(q1)
 
@@ -87,11 +87,9 @@ def r_zyzz(q0: cudaq.qubit,
 
 
 @cudaq.kernel
-def r_zzyz(q0: cudaq.qubit,
-           q1: cudaq.qubit,
-           q2: cudaq.qubit,
-           q3: cudaq.qubit,
-           theta: float):
+def r_zzyz(
+    q0: cudaq.qubit, q1: cudaq.qubit, q2: cudaq.qubit, q3: cudaq.qubit, theta: float
+):
     s(q2)
     h(q2)
 
@@ -102,11 +100,9 @@ def r_zzyz(q0: cudaq.qubit,
 
 
 @cudaq.kernel
-def r_zzzy(q0: cudaq.qubit,
-           q1: cudaq.qubit,
-           q2: cudaq.qubit,
-           q3: cudaq.qubit,
-           theta: float):
+def r_zzzy(
+    q0: cudaq.qubit, q1: cudaq.qubit, q2: cudaq.qubit, q3: cudaq.qubit, theta: float
+):
     s(q3)
     h(q3)
     r_zzzz(q0, q1, q2, q3, theta)
@@ -118,22 +114,22 @@ def get_interactions(N: int) -> (list, list):
     """
     Generates the interaction sets G2 and G4 based on the loop limits in Eq. 15.
     Returns standard 0-based indices as lists of lists of ints.
-   
+
     Args:
         N (int): Sequence length.
-       
+
     Returns:
         G2: List of lists containing two body term indices
         G4: List of lists containing four body term indices
     """
-   
+
     G2 = []
     G4 = []
 
     # --- Two-body terms ---
     for i in range(N - 2):
-        max_k = (N - i) // 2          # depends on i
-        for k in range(1, max_k + 1): # starts at 1 to avoid (i,i)
+        max_k = (N - i) // 2  # depends on i
+        for k in range(1, max_k + 1):  # starts at 1 to avoid (i,i)
             G2.append([i, i + k])
 
     # --- Four-body terms ---
@@ -149,9 +145,17 @@ def get_interactions(N: int) -> (list, list):
 
 
 @cudaq.kernel
-def trotterized_circuit(N: int, G2: list[list[int]], G4: list[list[int]], steps: int, dt: float, T: float, thetas: list[float]):
+def trotterized_circuit(
+    N: int,
+    G2: list[list[int]],
+    G4: list[list[int]],
+    steps: int,
+    dt: float,
+    T: float,
+    thetas: list[float],
+):
     """Equation B3 to create a trotterized circuit
-    
+
     N:      the size of the bit strings
     G2:     list of lists containing two body term indices
     G4:     list of lists containing four body term indices
@@ -174,7 +178,6 @@ def trotterized_circuit(N: int, G2: list[list[int]], G4: list[list[int]], steps:
             r_yz(reg[i], reg[j], -4.0 * theta)
             r_zy(reg[i], reg[j], -4.0 * theta)
 
-
         # 4-body block
         for quad in G4:
             a, b, c, d = quad[0], quad[1], quad[2], quad[3]
@@ -189,10 +192,10 @@ def trotterized_circuit(N: int, G2: list[list[int]], G4: list[list[int]], steps:
 def bitstring_convert(bitstring: str) -> np.ndarray:
     """
     Convert cudaq.sample output to {-1, +1} bitstrings
-    
+
     bitstring: the bitstring from a cudaq sample to convert
     """
-    return np.array([1 if b == '1' else -1 for b in bitstring])
+    return np.array([1 if b == "1" else -1 for b in bitstring])
 
 
 def quantum_population(
@@ -213,12 +216,12 @@ def quantum_population(
     # initialize parameters for cudaq samples
     dt = T / n_steps
     G2, G4 = get_interactions(N)
-    thetas =[]
+    thetas = []
     for step in range(1, n_steps + 1):
         t = step * dt
         theta_val = utils.compute_theta(t, dt, T, N, G2, G4)
         thetas.append(theta_val)
-        
+
     # generate samples
     samples = cudaq.sample(
         trotterized_circuit,
@@ -230,15 +233,16 @@ def quantum_population(
         T,
         thetas,
         shots_count=int(shots_count),
+        target="nvidia",
     )
 
     population = []
     for bitstring, count in samples.items():
-       for _ in range(count):
-           population.append(bitstring_convert(bitstring))
-           if len(population) >= popsize:
-               return population
-               
+        for _ in range(count):
+            population.append(bitstring_convert(bitstring))
+            if len(population) >= popsize:
+                return population
+
     return population
 
 
@@ -251,7 +255,7 @@ def qe_mts(population):
     N = len(population[0])
 
     # MTS results
-    return MTS(len(population), N, population0 = population)
+    return MTS(len(population), N, population0=population)
 
 
 def generate_qemts_data(Ns, popsize=100, T=1, n_steps=3, shots_count: int = 1000):
@@ -272,17 +276,17 @@ def generate_qemts_data(Ns, popsize=100, T=1, n_steps=3, shots_count: int = 1000
 
     for n in Ns:
         # run QE-MTS
-        quantum_init = quantum_population(popsize, T, n_steps, n, shots_count=shots_count)
-        quantum_final = MTS(len(quantum_init), n, population0=quantum_init, record_time=True)
+        quantum_init = quantum_population(
+            popsize, T, n_steps, n, shots_count=shots_count
+        )
+        quantum_final = MTS(
+            len(quantum_init), n, population0=quantum_init, record_time=True
+        )
 
         _, best_E_q, _, _, _, convergence_time = quantum_final
         best_E_q_list.append(best_E_q)
         time_q_list.append(convergence_time)
 
-    data = pd.DataFrame({
-        "N": Ns,
-        "best_E_q": best_E_q_list,
-        "time_q": time_q_list
-    })
-    
+    data = pd.DataFrame({"N": Ns, "best_E_q": best_E_q_list, "time_q": time_q_list})
+
     return data
